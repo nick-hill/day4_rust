750ce06e1c3b4af5e244743cb97959b319e4c159
Nikhil Mishra <nikhil.mishra@knoldus.in>
2019-07-12 18:37:34 +0530

    Commit - generics

--- src/generics.rs
+++ src/generics.rs
@@ -8,6 +8,16 @@ struct Points<T, U> {
     y : U,
 }
 
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+enum Option_f64 {
+    Some(f64),
+    None,
+}
+
 pub fn generic() {
 
     //single type
@@ -18,5 +28,9 @@ pub fn generic() {
     let integer = Points { x: 5, y: 10 };
     let float = Points{ x: 1.0, y: 4.0 };
     let float_and_integer = Points{ x: 1, y: 4.0 };
+
+    //Option
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
 
 }
